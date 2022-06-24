b981fb33de87149b3683fd7f14cf23825edb7d18
Roi Argaman <zebroi.arg@gmail.com>
2022-06-24 18:51:59 +0300

    more of the same

--- src/Units/Enemies/Trap.java
+++ src/Units/Enemies/Trap.java
@@ -1,7 +1,8 @@
 package Units.Enemies;
 
 public class Trap extends Enemy {
-    protected Trap(char tile, String name, int healthCapacity, int attack, int defense) {
-        super(tile, name, healthCapacity, attack, defense);
+
+    protected Trap(char tile, String name, int attack, int defense) {
+        super(tile, name, attack, defense);
     }
 }
