e39ef29d70bdd4505fc4e2fcdca5edba74766555
Roi Argaman <zebroi.arg@gmail.com>
2022-06-26 14:32:43 +0300

    change event register

--- src/Dungeons_and_Dragons/GameTicker.java
+++ src/Dungeons_and_Dragons/GameTicker.java
@@ -5,9 +5,4 @@ public interface GameTicker {
     public default void OnGameTick(){
         this.toString();
     }
-
-    public default void RegisterGameTick()
-    {
-        GameTickListener.RegisterGameTick(this);
-    }
 }
